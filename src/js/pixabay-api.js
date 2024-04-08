a874f63498a81debf91fbaaf30f511e7b4ae9a3b
KaterynaYelmanova <148703875+KaterynaYelmanova@users.noreply.github.com>
2024-04-08 15:50:13 +0300

    add 8-04

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,12 +1,18 @@
 import axios from 'axios';
 
+let currentPage = 1;
+
 export async function fetchImages(query) {
   query = encodeURIComponent(query);
 
   try {
     const response = await axios.get(
-      `https://pixabay.com/api/?key=43144570-5608d834234b71965ee211368&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
+      `https://pixabay.com/api/?key=43144570-5608d834234b71965ee211368&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=15`
     );
+    currentPage++;
+    if (response.data.totalHits === 0) {
+      currentPage = 1;
+    }
 
     return response.data;
   } catch (error) {

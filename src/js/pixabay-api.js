1bf83245bb567f7b3e39227b202a3be5c0928b4a
KaterynaYelmanova <148703875+KaterynaYelmanova@users.noreply.github.com>
2024-04-10 12:42:28 +0300

    js-12end

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,20 +1,19 @@
 import axios from 'axios';
 
-let currentPage = 1;
-
-export async function fetchImages(query) {
-  query = encodeURIComponent(query);
-
+export async function fetchImages(query, currentPage) {
   try {
-    const response = await axios.get(
-      `https://pixabay.com/api/?key=43144570-5608d834234b71965ee211368&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=15`
-    );
-    currentPage++;
-    if (response.data.totalHits === 0) {
-      currentPage = 1;
-    }
+    const params = {
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 15,
+      page: currentPage,
+      key: '43144570-5608d834234b71965ee211368',
+    };
 
-    return response.data;
+    const res = await axios.get('https://pixabay.com/api/?', { params });
+    return res.data;
   } catch (error) {
     console.error('Помилка запиту:', error.message);
   }
--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import { renderImages } from './js/render-functions';
 export const galleryList = document.querySelector('ul.gallery');
 let query = '';
 
+let page = 1;
+let maxPage = 0;
+const pageSize = 15;
+
 const displayLoader = () => {
   galleryList.innerHTML += '<span class="loader"></span>';
 };
@@ -21,11 +25,22 @@ const searchButton = document.getElementById('search-button');
 searchButton.addEventListener('click', async () => {
   if (query && query.length >= 3) {
     displayLoader();
+    page = 1;
 
     try {
-      const data = await fetchImages(query);
+      const data = await fetchImages(query, page);
+      maxPage = Math.ceil(data.totalHits / pageSize);
       renderImages(data);
+      const cardHeight = document.querySelector('.gallery-item');
+      const domRect = cardHeight.getBoundingClientRect();
+      window.scrollBy({
+        top: domRect.height * 2,
+        behavior: 'smooth',
+      });
+
       inputQuery.value = '';
+
+      checkBtnStatus();
     } catch (error) {
       iziToast.error({
         title: 'Error',
@@ -49,3 +64,42 @@ inputQuery.addEventListener('keypress', e => {
     searchButton.click();
   }
 });
+
+const loadMoreButton = document.querySelector('.next-page-btn');
+loadMoreButton.addEventListener('click', async () => {
+  page += 1;
+
+  try {
+    const data = await fetchImages(query, page);
+    renderImages(data);
+    inputQuery.value = '';
+    checkBtnStatus();
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      message: `Sorry, there are no images matching your search query. Please try again!`,
+      position: 'topRight',
+    });
+    inputQuery.value = '';
+  }
+});
+
+function showLoadMore() {
+  loadMoreButton.classList.remove('hidden');
+}
+function hideLoadMore() {
+  loadMoreButton.classList.add('hidden');
+}
+
+function checkBtnStatus() {
+  if (page >= maxPage) {
+    hideLoadMore();
+    iziToast.info({
+      title: 'Info',
+      message: `We're sorry, but you've reached the end of search results.`,
+      position: 'topRight',
+    });
+  } else {
+    showLoadMore();
+  }
+}
